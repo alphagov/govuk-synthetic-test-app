05783aa902929635cc7b54973577802d33192346
kentsanggds <ken.tsang@digital.cabinet-office.gov.uk>
2025-10-08 18:18:24 +0100

    Update ginkgo tests from code review
    - use log.Printf in place of fmt.Printf
    - update check on arm64 to use HaveKeyWithValue
    - use expect error in place of check on error response
    - check that there is no error from creating a request object

--- helpers/k8s_api_test.go
+++ helpers/k8s_api_test.go
@@ -2,10 +2,11 @@ package helpers_test
 
 import (
 	"fmt"
-	k8s_api "govuk-synthetic-test-app/helpers"
 	"log"
 	"net/http"
 	"os"
+
+	k8s_api "github.com/alphagov/govuk-synthetic-test-app/helpers"
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
@@ -15,24 +16,19 @@ var _ = Describe("Synthetic Test Assumed role", func() {
 	Context("when calling k8s api with apps namespace and pods kind", func() {
 		It("returns pods list and can access the first image value", func() {
 			podList, _ := k8s_api.GetPodList(os.Getenv("ENVIRONMENT_ACCOUNT_ID"), "apps")
-			fmt.Printf("First pod image: %+v, %+v\n", podList.Items[0].Labels["app"], podList.Items[0].Spec.Containers[0].Image)
+			log.Printf("First pod image: %s, %s\n", podList.Items[0].Labels["app"], podList.Items[0].Spec.Containers[0].Image)
 			Expect(podList.Items[0].Spec.Containers[0].Image).NotTo(BeNil())
 		})
 		It("returns pods list and all pods are running with arch arm64", func() {
 			podList, _ := k8s_api.GetPodList(os.Getenv("ENVIRONMENT_ACCOUNT_ID"), "apps")
 			Expect(podList.Items[0].Labels["app.kubernetes.io/arch"]).To(Equal("arm64"))
-			var allARM64 = true
+
 			for _, item := range podList.Items {
-				if val, ok := item.Labels["app.kubernetes.io/arch"]; ok {
-					if val != "arm64" {
-						fmt.Printf("Found non-arm64 pod: %+v\n", item.Labels["app"])
-						allARM64 = false
-					}
-				} else {
-					fmt.Printf("Found pod missing arch label: %+v\n", item.Name)
-				}
+				Expect(item.Labels).To(
+					HaveKeyWithValue("app.kubernetes.io/arch", "arm64"),
+					fmt.Sprintf("item %s is missing the app.kubernetes.io/arch label, or its value isn't 'arm64'", item.Name),
+				)
 			}
-			Expect(allARM64).To(Equal(true))
 		})
 	})
 
@@ -41,8 +37,7 @@ var _ = Describe("Synthetic Test Assumed role", func() {
 			func(namespace string) {
 				podList, err := k8s_api.GetPodList(os.Getenv("ENVIRONMENT_ACCOUNT_ID"), namespace)
 				Expect(podList).To(BeNil())
-				expected_string := fmt.Sprintf("Error got 403 status, retrieving https://kubernetes.default.svc/api/v1/namespaces/%+v/pods", namespace)
-				Expect(err.Error()).To(Equal(expected_string))
+				Expect(err).To(HaveOccurred())
 			},
 			Entry("for datagovuk", "datagovuk"),
 			Entry("for default", "default"),
@@ -59,9 +54,8 @@ var _ = Describe("Synthetic Test Assumed role", func() {
 				url := k8s_api.API_SERVER + "/apps/pods"
 
 				req, err := http.NewRequest(http_method, url, nil)
-				if err != nil {
-					log.Fatal(err)
-				}
+				Expect(err).NotTo(HaveOccurred())
+
 				req.Header.Set("Authorization", "Bearer "+token)
 				req.Header.Set("Accept", "application/yaml")
 
@@ -97,8 +91,8 @@ var _ = Describe("Synthetic Test Assumed role", func() {
 				func(environment_account_id string) {
 					podList, err := k8s_api.GetPodList(environment_account_id, "apps")
 					Expect(podList).To(BeNil())
-					Expect(err.Error()).To(ContainSubstring(fmt.Sprintf("User: arn:aws:sts::%+v:assumed-role/synthetic-test-assumer", k8s_api.STAGING_AWS_ACCOUNT_ID)))
-					Expect(err.Error()).To(ContainSubstring(fmt.Sprintf("is not authorized to perform: sts:AssumeRole on resource: arn:aws:iam::%+v:role/synthetic-test-assumed", environment_account_id)))
+					Expect(err.Error()).To(ContainSubstring(fmt.Sprintf("User: arn:aws:sts::%s:assumed-role/synthetic-test-assumer", k8s_api.STAGING_AWS_ACCOUNT_ID)))
+					Expect(err.Error()).To(ContainSubstring(fmt.Sprintf("is not authorized to perform: sts:AssumeRole on resource: arn:aws:iam::%s:role/synthetic-test-assumed", environment_account_id)))
 				},
 				Entry("for integration", k8s_api.INTEGRATION_AWS_ACCOUNT_ID),
 				Entry("for production", k8s_api.PRODUCTION_AWS_ACCOUNT_ID),
@@ -110,8 +104,8 @@ var _ = Describe("Synthetic Test Assumed role", func() {
 				func(environment_account_id string) {
 					podList, err := k8s_api.GetPodList(environment_account_id, "apps")
 					Expect(podList).To(BeNil())
-					Expect(err.Error()).To(ContainSubstring(fmt.Sprintf("User: arn:aws:sts::%+v:assumed-role/synthetic-test-assumer", k8s_api.INTEGRATION_AWS_ACCOUNT_ID)))
-					Expect(err.Error()).To(ContainSubstring(fmt.Sprintf("is not authorized to perform: sts:AssumeRole on resource: arn:aws:iam::%+v:role/synthetic-test-assumed", environment_account_id)))
+					Expect(err.Error()).To(ContainSubstring(fmt.Sprintf("User: arn:aws:sts::%s:assumed-role/synthetic-test-assumer", k8s_api.INTEGRATION_AWS_ACCOUNT_ID)))
+					Expect(err.Error()).To(ContainSubstring(fmt.Sprintf("is not authorized to perform: sts:AssumeRole on resource: arn:aws:iam::%s:role/synthetic-test-assumed", environment_account_id)))
 				},
 				Entry("for staging", k8s_api.STAGING_AWS_ACCOUNT_ID),
 				Entry("for production", k8s_api.PRODUCTION_AWS_ACCOUNT_ID),
