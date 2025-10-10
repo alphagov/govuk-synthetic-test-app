750afa0c18951a89c7db0e559ebf1ea9da653a74
kentsanggds <ken.tsang@digital.cabinet-office.gov.uk>
2025-10-10 16:33:00 +0100

    Refactor code to use cluster credentials
    Use the cluster endpoint and cluster certificate to access the k8s API rather than the default endpoint and cert for the cluster as this will only work with the current cluster.

--- helpers/k8s_api.go
+++ helpers/k8s_api.go
@@ -4,12 +4,22 @@ import (
 	"context"
 	"crypto/tls"
 	"crypto/x509"
+	"encoding/base64"
 	"fmt"
 	"io"
+	"math/rand"
 	"net/http"
 	"net/url"
 	"os"
+	"strconv"
+	"time"
 
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/credentials"
+	"github.com/aws/aws-sdk-go-v2/service/eks"
+	"github.com/aws/aws-sdk-go-v2/service/sts"
+	stsTypes "github.com/aws/aws-sdk-go-v2/service/sts/types"
 	"sigs.k8s.io/aws-iam-authenticator/pkg/token"
 
 	corev1 "k8s.io/api/core/v1"
@@ -17,7 +27,6 @@ import (
 	"k8s.io/apimachinery/pkg/runtime/serializer"
 )
 
-const API_SERVER string = "https://kubernetes.default.svc/api/v1/namespaces"
 const CERT_PATH string = "/var/run/secrets/kubernetes.io/serviceaccount/ca.crt"
 const INTEGRATION_AWS_ACCOUNT_ID string = "210287912431"
 const STAGING_AWS_ACCOUNT_ID string = "696911096973"
@@ -25,6 +34,8 @@ const PRODUCTION_AWS_ACCOUNT_ID string = "172025368201"
 const INTEGRATION string = "integration"
 const STAGING string = "staging"
 const PRODUCTION string = "production"
+const CLUSTER_ID string = "govuk"
+const REGION string = "eu-west-1"
 
 func CheckRunningInK8s() (bool, error) {
 	if _, err := os.Stat(CERT_PATH); err != nil {
@@ -51,37 +62,91 @@ func GetEnvironmentAccountID(environment string) (string, error) {
 	}
 }
 
-func GetK8sClient(ctx context.Context, environment string) (*http.Client, string, error) {
+type K8sClient struct {
+	Client          *http.Client
+	Token           string
+	ClusterEndpoint string
+}
+
+func (k *K8sClient) Get(url string) (*http.Response, error) {
+	req, err := http.NewRequest("GET", fmt.Sprintf("%s/api/v1/namespaces/%s", k.ClusterEndpoint, url), nil)
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Set("Authorization", "Bearer "+k.Token)
+	req.Header.Set("Accept", "application/yaml")
+
+	return k.Client.Do(req)
+}
+
+func GetK8sClient(ctx context.Context, environment string) (*K8sClient, error) {
 	running_in_k8s, err := CheckRunningInK8s()
 	if err != nil {
-		return nil, "", err
+		return nil, err
 	} else if !running_in_k8s {
-		return nil, "", nil
+		return nil, nil
 	}
 
 	g, err := token.NewGenerator(false, false)
 	if err != nil {
-		return nil, "", err
+		return nil, err
 	}
 
 	environment_account_id, err := GetEnvironmentAccountID(environment)
 	if err != nil {
-		return nil, "", err
+		return nil, err
 	}
 
 	tk, err := g.GetWithOptions(ctx, &token.GetTokenOptions{
-		Region:        "eu-west-1",
-		ClusterID:     "govuk",
+		Region:        REGION,
+		ClusterID:     CLUSTER_ID,
 		AssumeRoleARN: fmt.Sprintf("arn:aws:iam::%s:role/synthetic-test-assumed", environment_account_id),
 		SessionName:   "GovUKSyntheticTestApp",
 	})
 	if err != nil {
-		return nil, "", err
+		return nil, err
 	}
 
-	caCert, err := os.ReadFile(CERT_PATH)
+	cfg, err := config.LoadDefaultConfig(ctx, config.WithRegion(REGION))
 	if err != nil {
-		return nil, "", err
+		return nil, err
+	}
+	sourceAccount := sts.NewFromConfig(cfg)
+
+	rand.Seed(time.Now().UnixNano())
+	response, err := sourceAccount.AssumeRole(
+		ctx,
+		&sts.AssumeRoleInput{
+			RoleArn:         aws.String(fmt.Sprintf("arn:aws:iam::%s:role/synthetic-test-assumed", environment_account_id)),
+			RoleSessionName: aws.String("GOVUK-Synthetic-Test-Assumed-" + strconv.Itoa(10000+rand.Intn(25000))),
+		})
+	if err != nil {
+		return nil, err
+	}
+	var assumedRoleCreds *stsTypes.Credentials = response.Credentials
+
+	cfg, err = config.LoadDefaultConfig(
+		ctx,
+		config.WithRegion(REGION),
+		config.WithCredentialsProvider(
+			credentials.NewStaticCredentialsProvider(
+				*assumedRoleCreds.AccessKeyId,
+				*assumedRoleCreds.SecretAccessKey,
+				*assumedRoleCreds.SessionToken)))
+	if err != nil {
+		return nil, err
+	}
+
+	eks_client := eks.NewFromConfig(cfg)
+
+	cluster, err := eks_client.DescribeCluster(ctx, &eks.DescribeClusterInput{Name: aws.String(CLUSTER_ID)})
+	if err != nil {
+		return nil, err
+	}
+
+	caCert, err := base64.StdEncoding.DecodeString(*cluster.Cluster.CertificateAuthority.Data)
+	if err != nil {
+		return nil, err
 	}
 	caCertPool := x509.NewCertPool()
 	caCertPool.AppendCertsFromPEM(caCert)
@@ -94,27 +159,21 @@ func GetK8sClient(ctx context.Context, environment string) (*http.Client, string
 		},
 	}
 
-	return client, tk.Token, nil
+	return &K8sClient{
+		Client:          client,
+		Token:           tk.Token,
+		ClusterEndpoint: *cluster.Cluster.Endpoint,
+	}, nil
 }
 
 func GetK8sAPIData(ctx context.Context, environment string, namespace string, resource_type string) ([]byte, error) {
-	client, token, err := GetK8sClient(ctx, environment)
+	client, err := GetK8sClient(ctx, environment)
 	if err != nil {
 		return nil, err
 	}
+	url, err := url.JoinPath(namespace, resource_type)
 
-	url, err := url.JoinPath(API_SERVER, namespace, resource_type)
-	if err != nil {
-		return nil, err
-	}
-	req, err := http.NewRequest("GET", url, nil)
-	if err != nil {
-		return nil, err
-	}
-	req.Header.Set("Authorization", "Bearer "+token)
-	req.Header.Set("Accept", "application/yaml")
-
-	resp, err := client.Do(req)
+	resp, err := client.Get(url)
 	if err != nil {
 		err = fmt.Errorf("Error: %v, retrieving %v", err, url)
 		return nil, err
